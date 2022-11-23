1ff1752cfe1e95e413d1c115d57beac49880661d
Armando.Rod <armando.rod79@gmail.com>
2022-11-22 17:26:46 -0800

    added base files and started .js files within components folder

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+function Navigaiton () {
+    return (
+        <nav>
+            <a href="">About Me</a>
+            <a href="">Portfolio</a>
+            <a href="">Contact</a>
+            <a href="">Resume</a>
+        </nav>
+    );
+
+}
+export default Navigaiton;
